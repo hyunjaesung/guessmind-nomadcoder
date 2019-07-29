ae3b6211ecaef140967e900b2fe4b78ce2d194a5
stevesung <steveysung91@gmail.com>
2019-07-30 07:39:52 +0900

    server setting

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
+import { join } from "path";
 import express from "express";
+import socketId from "socket.io";
+
+const PORT = "4000";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`Server Listening now on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
